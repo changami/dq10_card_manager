6430cdd7b905c576238b30a609fc3c258f557d5e
chan_gami <babyryo@msn.com>
2014-08-28 04:00:10 +0900

    バリデーションとテストの追加

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class CardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :cards
+
+  test "card attributes must not be empty" do
+    card = Card.new
+    assert card.invalid?
+    assert card.errors[:name].any?
+    assert card.errors[:remaining_hour].any?
+    assert card.errors[:own_player].any?
+  end
+
+  test "card remaining_hour must be positive" do
+    card = Card.new(
+        name: "Test Card",
+        own_player: "chan_gami"
+    )
+
+    card.remaining_hour = -1
+    assert card.invalid?
+    assert_equal "must be greater than or equal to 1",
+                 card.errors[:remaining_hour].join('; ')
+
+    card.remaining_hour = 0
+    assert card.invalid?
+    assert_equal "must be greater than or equal to 1",
+                 card.errors[:remaining_hour].join('; ')
+
+    card.remaining_hour = 721
+    assert card.invalid?
+    assert_equal "must be less than or equal to 720",
+                 card.errors[:remaining_hour].join('; ')
+
+    card.remaining_hour = 1
+    assert card.valid?
+
+    card.remaining_hour = 720
+    assert card.valid?
+  end
+
+  test "card can save a duplicate data" do
+    card = Card.new(
+        name: cards(:atlas).name,
+        remaining_hour: cards(:atlas).remaining_hour,
+        own_player: cards(:atlas).own_player
+    )
+    assert card.save
+  end
 end
