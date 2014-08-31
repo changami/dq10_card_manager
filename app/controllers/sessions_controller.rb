279f33cd97742c2a24a5e6cfe13cf36e81c40788
chan_gami <babyryo@msn.com>
2014-08-31 19:26:23 +0900

    ログイン処理とルーティング(仮)を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    team = Team.find_by_name(params[:name])
+    if team and team.authenticate(params[:password])
+      session[:team_id] = team.id
+      redirect_to cards_url
+    else
+      redirect_to login_url, alert: "無効なチーム / パスワードの組み合わせです"
+    end
   end
 
   def destroy
+    session[:team_id] = nil
+    redirect_to login_url, alert: "ログアウト"
   end
 end
